7999b271030320be7d8846a567bfbd7d1158f13d
Sanchezdav <sanchez.dav90@gmail.com>
2014-06-22 14:37:20 -0500

    Primer Commit

---
+++ zapateria/app/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Perfil(models.Model):
+	usuario = models.OneToOneField(User)
+	estado = models.CharField(max_length=255)
+	municipio = models.CharField(max_length=255)
+	colonia = models.CharField(max_length=255)
+	calle = models.CharField(max_length=255)
+	numero = models.IntegerField()
+	codigo_postal = models.IntegerField()
+	telefono = models.CharField(max_length=15)
+
+class Tipo(models.Model):
+	tipo = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return self.tipo
+
+class Color(models.Model):
+	color = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return self.color
+
+class Material(models.Model):
+	material = models.CharField(max_length=255)
+	color = models.ForeignKey(Color)
+
+	def __unicode__(self):
+		return self '%s %s' % (self.material, self.color)
+
+class Adorno(models.Model):
+	adorno = models.CharField(max_length=255)
+	color = models.ForeignKey(Color)
+
+	def __unicode__(self):
+		return self '%s %s' % (self.adorno, self.color)
+
